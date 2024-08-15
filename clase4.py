a80912dd836b2c4d92df9056512c1f9362727477
Israel Garcia <isragarciahdez@gmail.com>
2024-08-15 12:33:51 -0600

    Manipulación de cadenas de texto en python

---
+++ clase4.py
@@ -0,0 +1,9 @@
+name = 'ISRA mar'
+lastname = '  Garcia  Hern  '
+print(name * 5)
+print(name + ' ' + lastname)
+print(len(lastname))
+print(name.lower())
+print(name.upper())
+print(name.capitalize())
+print(lastname.strip())
