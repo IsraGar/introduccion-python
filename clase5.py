4a2b35b0e7709ea5321fc9c7e8a0848c6f2eb245
Israel Garcia <isragarciahdez@gmail.com>
2024-08-15 12:41:17 -0600

    Enteros, flotantes y booleanos

---
+++ clase5.py
@@ -0,0 +1,13 @@
+x = 10
+y = 5.678
+is_true = True
+is_false = False
+#z = 1.2e6
+#a = 1.2e-6
+print(type(x))
+print(type(y))
+print(x + x)
+print(x + y)
+print(y + y)
+print(is_true)
+print(is_false)
