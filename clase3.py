6a63b6e01b892cf1cb16f44261be7df32af36775
Israel Garcia <isragarciahdez@gmail.com>
2024-08-15 13:22:35 -0600

    Operaciones con cadenas de texto

--- clase3.py
+++ clase3.py
@@ -4,3 +4,9 @@ nombre = "Isra"
 
 print(saludo_1)
 print(nombre)
+print("Nunca", "pares", "de", "aprender")
+print("Nunca", "pares", "de", "aprender", sep=", ")
+print("Nunca", end=" ")
+print("pares de aprender")
+print(f"Saludo: {saludo_1}, Nombre: {nombre}")
+print("Ruta de archivo: ", "C:\\Users\\user1\\docs\\test")
