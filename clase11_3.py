0fdc91c1fd84d1c7f2bc878e5bddf4465f2395a2
Israel Garcia <isragarciahdez@gmail.com>
2024-08-19 16:43:35 -0600

    Estructuras condicionales

---
+++ clase11_3.py
@@ -0,0 +1,10 @@
+is_member = True
+age = 15
+
+if is_member:
+    if age >= 15:
+        print("Tienes acceso ya que eres miembro y con edad suficiente")
+    else:
+        print("Edad no suficiente")
+else:
+    print("No eres miembro")
